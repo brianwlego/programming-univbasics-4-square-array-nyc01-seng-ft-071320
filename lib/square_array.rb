d1eb21aaf63af068df3f5f9d33dff965c0b69bf9
Brian Lego <legobw@gmail.com>
2020-06-22 23:34:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ array = [1,2,3]
 new_array = []
 def square_array(array)
   array.length.times do |index|
-    square_number = array[index]*array[index]
+    square_number = (array[index]*array[index])
     new_array.push(square_number)
     puts new_array
   end 
